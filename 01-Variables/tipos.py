dd90d12ba0e696c6612b5923107f145fc7da767c
MatiasCordich <m.scordich@gmail.com>
2024-01-12 23:05:36 -0300

    Variables terminado

---
+++ 01-Variables/tipos.py
@@ -0,0 +1,121 @@
+"""
+
+Los tipos de datos hace referencia al tipo de dato que puede contener una variable. 
+
+En Pytnon existen los siguientes tipos de datos
+
+"""
+
+# STRING (STR)
+"""
+Practicamente es un texto, o tambien como se dice, una cadena de caracteres
+"""
+
+string = "Hola a todos"
+print(string)
+
+# ENTERO (INT)
+"""
+Hace referencia a los numeros enteros
+"""
+
+entero = 23
+print(entero)
+
+# FLOTANTE (FLOAT)
+"""
+Hace referencia a los numeros decimales
+"""
+
+flotante = 7.8
+print(flotante)
+
+# BOOLEANO (TRUE O FALSE)
+"""
+Hace referencia a solo dos valores posibles VERDARO (True) o FALSO (False)
+"""
+
+boleano = True
+print(boleano)
+
+# LISTA (LIST)
+"""
+Es un tipo de dato mas complejo que permite guardar una coleccion de datos. 
+Las listas tienen la siguiente caracteristica
+- Son ordenadas
+- Son editables
+- Pueden contener distintos tipos de datos
+- Puede contener elementos duplicados
+"""
+
+lista = ["Banana", "Manzana", "Pera", "Kiwi"]
+print(lista)
+
+listaVariada = ["Queso", 43, 2, "Atun", "Cerveza", True]
+print(listaVariada)
+
+# TUPLA (TUPLE)
+"""
+Es igual a la lista pero a diferencia de esta, su contenido es INMUTABLE, es decir, NO PUEDE CAMBIAR
+"""
+
+tupla = ("Hola", "Soy", "Matias")
+print(tupla)
+
+# DICCIONARIO (DICT)
+"""
+Los diccionarios en Python son una estructura de datos donde permite organizar los datos y organizarlos de manera mas flexible. 
+A diferencia de las tuplas o listas, los diccionarios NO ESTAN INDEXADOS. 
+Los elementos de un diccionario estan estructurados en conjuntos de pares clave valor
+("clave1": valor)
+"""
+
+diccionario = {
+    "nombre": "Pepe",
+    "apellido": "Esculapio",
+    "edad": 23  
+}
+print(diccionario)
+
+# SABER EL TIPO DE DATO DE UNA VARIABLE
+
+"""
+Para saber el tipo de dato de una variable, simplemente utilizamos la funcion type(variable)
+"""
+
+print(type(string))
+print(type(entero))
+print(type(flotante))
+print(type(lista))
+print(type(tupla))
+print(type(diccionario))
+
+# CONVERTIR DE UN TIPO DE DATO A OTRO
+
+"""
+Supongamos que tenemos este ejemplo
+"""
+
+texto = "Hola soy un texto"
+numerito = 123
+
+# print(texto + " " + numerito) --> ESTO DA ERROR
+
+"""
+Si yo quiero correr el programa me va a dar error porque no se pueden concatenar el valor de texto con el valor de numerito porque numerito es de tipo int. Para resolver esto debemos utilizar el str()
+"""
+
+numerito_a_string = str(123)
+
+print(texto + " " + numerito_a_string)
+
+"""
+Tambien podemos convertir dicho variable a otro tipo de dato con int(), float()
+"""
+
+numerito_a_string = int(123)
+print(type(numerito_a_string))
+
+numerito_a_string = float(123)
+print(type(numerito_a_string))
+print(numerito_a_string)
---
+++ 01-Variables/variables.py
@@ -0,0 +1,59 @@
+"""
+
+Una variable es un contenedor de informacion, es decir, dentro de la misma se guardara un dato. 
+Las variables pueden ser de distintos tipos de datos.
+
+"""
+
+# String
+texto = "Hola mundo"
+print(texto)
+
+
+# Concatenacion
+
+"""
+
+La concatenacion es una forma de poder unir distintos valores de diferentes variables. 
+Existen dos formas de concatenar 
+
+"""
+
+nombre = "Pepe"
+apellido = "Esculapio"
+
+# Primera forma
+
+"""
+
+Esta es la forma es la mas comun donde se concatenan mediante los signo de suma, pero OJO que en la concatenacion de strings tambien es necesario sumar los espacios ya que cuentan como caracteres. 
+
+"""
+print(nombre + " " + apellido)
+
+# Segunda forma
+"""
+
+Esta segunda forma es mas facil y sencilla porque simplemente tenemos que poner las variables cuyo valores tienen onde comenzamos con la letra "f" y seguido entre comillas los valores de cada variable, dichos valores deberan estar entre llaves {}
+
+"""
+print(f"{nombre} {apellido}")
+
+# Concatenacion insertando valores
+"""
+
+Tambien se pueden insertar valores cuando se muestra por consola un texto informativo donde se tiene que concatenar la cadena de texto con el valor de la variable. 
+Para realizar eso tengo que utilizar la funcion .format()
+
+"""
+
+edad = 21
+
+print("Hola, me llamo {} {} y tengo {} anios".format(nombre, apellido, edad))
+
+"""
+
+Como vemos en cada espacio donde hay llaves {} seran llenados con lo que pongamos en format()
+OJO es importante el orden para que el texto tenga sentido
+
+"""
