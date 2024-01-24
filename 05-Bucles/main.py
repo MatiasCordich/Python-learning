bee65045cab8c0cb69300d367346fd15a24b90d8
MatiasCordich <m.scordich@gmail.com>
2024-01-23 23:57:43 -0300

    Bucle FOR terminado

--- 05-Bucles/main.py
+++ 05-Bucles/main.py
@@ -28,15 +28,32 @@ contador = 0
 
 for contador in range(5):
     print(contador)
+    # 0, 1, 2, 3, 4
 
 """
-Como vemos al establecer una variable CONTADOR, cuyo valor empieza en 0, al utilizar range lo que hago es incrementar 5 veces dicho valor
+Como vemos al establecer una variable CONTADOR, cuyo valor empieza en 0, al utilizar el range(5) estoy diciendo que el valor maximo que puede tomar la variable CONTADOR es 4. 
 
 0 primera iteracion
 1 segunda iteracion
 2 tercera iteracion
 3 cuarta iteracion
 4 quinta iteracion
+"""
+
+"""
+Tambien a la funcion range podemos agregarle mas parametros de la sigiuente manera
+"""
+
+for i in range(0,10,2):
+    print(i)
+    # 0, 2, 4, 6, 8
+
+"""
+Cada paramentro indica lo siguiente
+Se toma primero como variable la variable i
+0: Indica el valor inicial que va a tener la variable i, en este caso 0
+10: Indica el valor maximo que puede tomar i, en este caso sera desde el 0 hasta el 9. CUIDADO!!!! A LA HORA DE HACER LA ITERACION EL LIMITE ES 10 PERO NO SE INCLUYE
+2: por cada iteracion el valor de i va a ir incrementando de 2 en 2.
 """
 
 # USANDO EL FOR PARA UNA LISTA
