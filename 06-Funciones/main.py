924203e85338b43331f802ec0edf84e1003a214a
MatiasCordich <m.scordich@gmail.com>
2024-01-28 22:35:21 -0300

    Funciones bloque agregado

---
+++ 06-Funciones/main.py
@@ -0,0 +1,20 @@
+#FUNCIONES
+"""
+Una funcion es un conjunto agrupado de instrucciones que pueden realizarse invocando la funcion tantas veces como sea necesario. 
+
+Para definiar una funcion utilizamos la palabra reservada def 
+
+def nombreDeMiFuncion(parametros):
+    BLOQUE / CONJUNTO DE INSTRUCIONES
+
+nombreDeMiFuncion(mi_parametro)
+
+No es necesario que las funciones tenga parametros, mas adelante se explica que son los parametros
+"""
+
+# DEFINIR FUNCION
+def mostarSaludo():
+    print("Hola")
+
+# INVOCAR FUNCION
+mostarSaludo()
