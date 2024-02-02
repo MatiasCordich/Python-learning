716d04b6b6e8cf93cd7a76835194d97db99bcf86
MatiasCordich <m.scordich@gmail.com>
2024-02-01 21:45:33 -0300

    Parametros

--- 06-Funciones/main.py
+++ 06-Funciones/main.py
@@ -18,3 +18,41 @@ def mostarSaludo():
 
 # INVOCAR FUNCION
 mostarSaludo()
+
+# PARAMETROS
+"""
+Supongamos que tenemos una funcio donde me pide mostrar la edad
+"""
+
+def mostrarEdad():
+    print("Tu edad es de 27 años")
+
+mostrarEdad()
+
+"""
+El problema surje cuando quiero mostrar una edad dinámica, es decir, lo que se muestre al utilizar el print sea un numero diferente a 27. Para eso existen los parametros.
+
+Los parámetros son datos que recibe una función y que servirán para que el bloque de código pueda funcionar con los datos recibidos por la función. 
+Aca lo vemos en un ejemplo
+"""
+
+def mostrarEdadParametro(edad):
+    print(f"Tu edad es de {edad} años")
+
+"""
+Como vemos, al poner EDAD como parametro estamso diciendo que esa función recibe un valor y que dicho valor será usado para la ejecución del código, en este caso mostrar por pantalla el numero que ingresamos. 
+"""
+
+mostrarEdadParametro(34)
+mostrarEdadParametro(21)
+mostrarEdadParametro(55)
+
+# EJEMPLO 2. FUNCION CON PARAMETROS MEDIANTE INPUT
+
+def presentarPersona(nombre, edad):
+    print(f"Hola, mi nombre es {nombre} y mi edad es de {edad} años")
+
+nombre_ingresado = input("Ingrese su nombre: ")
+edad_ingresada = int(input("Ingrese su edad: "))
+
+presentarPersona(nombre_ingresado, edad_ingresada)
