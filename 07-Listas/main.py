e40671ca818b0a7efcd03420239b195b9534bf8d
MatiasCordich <m.scordich@gmail.com>
2024-02-04 22:28:53 -0300

    LISTAS en Python

---
+++ 07-Listas/main.py
@@ -0,0 +1,8 @@
+# LISTAS
+"""
+Las listas en Python son como los Arrays o arreglos en otros lenguajes. 
+Entonces las listas son un conjunto de datos o valores bajo un unico nombre. 
+Para poder acceder a esos valores podemos utilizar un indice numero
+"""
+
+peliculas = ["Batman", "Spiderman", "Avengers"]
